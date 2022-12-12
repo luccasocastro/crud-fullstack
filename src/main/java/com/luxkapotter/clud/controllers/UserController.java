8273a36f6b85d216cf1e79371c3dd36cf187d649
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-12-11 22:39:45 -0300

    criando UserController

---
+++ src/main/java/com/luxkapotter/clud/controllers/UserController.java
@@ -0,0 +1,57 @@
+package com.luxkapotter.clud.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luxkapotter.clud.entities.User;
+import com.luxkapotter.clud.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	
+	@Autowired
+	private UserService service;
+	
+	@GetMapping
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return new ResponseEntity<List<User>>(list, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> findById(@PathVariable Long id) {
+		Optional<User> obj = service.findById(id);
+		return new ResponseEntity<Optional<User>>(obj, HttpStatus.OK);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		User user = service.insert(obj);
+		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return new ResponseEntity<User>(obj, HttpStatus.OK);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+}
