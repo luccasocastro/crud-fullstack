e82dadbca99602e85742f25fb12e1a8f342f82e7
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-12-11 21:59:15 -0300

    first commit

---
+++ src/main/java/com/luxkapotter/clud/CludApplication.java
@@ -0,0 +1,13 @@
+package com.luxkapotter.clud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CludApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CludApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luxkapotter/clud/CludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxkapotter.clud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
